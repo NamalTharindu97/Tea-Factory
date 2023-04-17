e85237efdd1618216d49c61743d48bf17b27e29d
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-17 21:20:12 +0530

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  "/api/v1/tea-factory/employees",
+  require("../backend/routes/employeeRoutes")
+);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
