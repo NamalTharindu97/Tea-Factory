ca39f0ba7d8d8543094d5bf287e761aa884b66e9
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-17 21:35:33 +0530

    feature/namal/employeeroutes

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -5,4 +5,22 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "get all contacts" });
 });
 
+router.route("/:id").get((req, res) => {
+  res
+    .status(200)
+    .json({ message: `get employee details employee :  ${req.params.id}` });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "create employee" });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `update employee : ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `employee deleted : ${req.params.id}` });
+});
+
 module.exports = router;
