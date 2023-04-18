fd2fdc7367eb7a4095904bcf2b025f27d33320dc
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-18 12:30:26 +0530

    validate token handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const dbConnection = require("./config/dbConnection");
+const validateToken = require("./middleware/validateTokenHandler");
 const dotenv = require("dotenv").config();
 
 dbConnection();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1/tea-factory/employees", require("../backend/routes/employeeRoutes"));
 app.use(errorHandler);
+app.use(validateToken);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
