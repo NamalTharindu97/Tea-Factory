165fdc95732b5d004517167e01fe1185bb771ad6
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-18 20:51:33 +0530

    frontend role base navigation

---
+++ frontend/src/pages/employee/empAdminPanal.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function empAdminPanal() {
+  return <div>empAdminPanal</div>;
+}
---
+++ frontend/src/pages/inventory/inventoryAdminPanal.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function inventoryAdminPanal() {
+  return <div>inventoryAdminPanal</div>;
+}
