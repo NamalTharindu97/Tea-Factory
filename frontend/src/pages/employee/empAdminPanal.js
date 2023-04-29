430e3b7382e15567a5dcfef93c9a73dff269b3a1
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-29 12:00:55 +0530

    change scroll bar css

--- frontend/src/pages/employee/empAdminPanal.js
+++ frontend/src/pages/employee/empAdminPanal.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./empPanal.css";
 
 export default function empAdminPanal() {
-  return <div>empAdminPanal</div>;
+  return <div className="empAdminPanal">empAdminPanal</div>;
 }
