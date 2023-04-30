224a1157883b1a7450e88f80c074b5954441ffb7
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-30 23:59:05 +0530

    add table data

--- frontend/src/pages/employee/EmployeeInfo/EmployeeInfo.js
+++ frontend/src/pages/employee/EmployeeInfo/EmployeeInfo.js
@@ -1,44 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SideBar } from "../../../components/EmployeCo/sidebar/SideBar";
 import { NavBar } from "../../../components/EmployeCo/navBar/NavBar";
 import { DataGrid } from "@mui/x-data-grid";
 import "./employeeInfo.scss";
+import axios from "axios";
 
 const columns = [
   { field: "id", headerName: "ID", width: 100 },
-  { field: "firstName", headerName: "First name", width: 200 },
-  { field: "lastName", headerName: "Last name", width: 200 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "phone", headerName: "Phone", width: 200 },
+  { field: "gender", headerName: "Gender", width: 200 },
+  { field: "age", headerName: "Age", width: 200 },
+  { field: "role", headerName: "Role", width: 200 },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
+    field: "createdAt",
+    headerName: "Created At",
     width: 200,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 400,
-    valueGetter: (params) => `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "updatedAt",
+    headerName: "Updated At",
+    width: 200,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-];
-
 export const EmployeeInfo = () => {
+  const [empData, setEmpData] = useState([]);
+
+  useEffect(() => {
+    const getEmployee = async () => {
+      try {
+        const response = await axios.get("/employees/");
+        setEmpData(response.data.map((emp) => ({ ...emp, id: emp._id })));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getEmployee();
+  }, []);
+
   return (
     <div className="EmployeeInfo">
       <SideBar />
       <div className="info-container">
         <NavBar />
         <div className="data-grid">
-          <DataGrid rows={rows} columns={columns} paginationModel={{ page: 0, pageSize: 25 }} style={{ color: "white" }} />
+          <DataGrid rows={empData} columns={columns} paginationModel={{ page: 0, pageSize: 25 }} />
         </div>
       </div>
     </div>
