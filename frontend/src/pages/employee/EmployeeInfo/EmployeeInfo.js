02b4f5c34351a50b0109d3aa6025d7aef6987575
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-01 15:03:07 +0530

    set update values to coloms

--- frontend/src/pages/employee/EmployeeInfo/EmployeeInfo.js
+++ frontend/src/pages/employee/EmployeeInfo/EmployeeInfo.js
@@ -4,62 +4,46 @@ import { NavBar } from "../../../components/EmployeCo/navBar/NavBar";
 import { DataGrid } from "@mui/x-data-grid";
 import "./employeeInfo.scss";
 import axios from "axios";
-
-const handleUpdate = (params) => {
-  // const rowId = params.id; // get the MongoDB _id field
-};
-
-const handleDelete = async (params) => {
-  const rowId = params.id; // get the MongoDB _id field
-  try {
-    const response = await axios.delete(`/employees/${rowId}`);
-    if (!response) {
-      console.log("not response from delete btn");
-    } else {
-      window.location.reload();
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const columns = [
-  { field: "name", headerName: "Name", width: 220, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  { field: "email", headerName: "Email", width: 250, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  { field: "phone", headerName: "Phone", width: 150, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  { field: "gender", headerName: "Gender", width: 100, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  { field: "age", headerName: "Age", width: 100, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  { field: "role", headerName: "Role", width: 180, headerClassName: "custom-header", cellClassName: "custom-cell" },
-  {
-    field: "update",
-    headerName: "Update",
-    sortable: false,
-    width: 100,
-    headerClassName: "custom-header",
-    cellClassName: "custom-cell",
-    renderCell: (params) => (
-      <button className="update-button" onClick={() => handleUpdate(params)}>
-        Update
-      </button>
-    ),
-  },
-  {
-    field: "delete",
-    headerName: "Delete",
-    sortable: false,
-    width: 100,
-    headerClassName: "custom-header",
-    cellClassName: "custom-cell",
-    renderCell: (params) => (
-      <button className="delete-button" onClick={() => handleDelete(params)}>
-        Delete
-      </button>
-    ),
-  },
-];
+import { useNavigate } from "react-router-dom";
 
 export const EmployeeInfo = () => {
   const [empData, setEmpData] = useState([]);
+  const navigate = useNavigate();
+
+  const columns = [
+    { field: "name", headerName: "Name", width: 220, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    { field: "email", headerName: "Email", width: 250, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    { field: "phone", headerName: "Phone", width: 150, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    { field: "gender", headerName: "Gender", width: 100, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    { field: "age", headerName: "Age", width: 100, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    { field: "role", headerName: "Role", width: 180, headerClassName: "custom-header", cellClassName: "custom-cell" },
+    {
+      field: "update",
+      headerName: "Update",
+      sortable: false,
+      width: 100,
+      headerClassName: "custom-header",
+      cellClassName: "custom-cell",
+      renderCell: (params) => (
+        <button className="update-button" onClick={() => handleUpdate(params)}>
+          Update
+        </button>
+      ),
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      width: 100,
+      headerClassName: "custom-header",
+      cellClassName: "custom-cell",
+      renderCell: (params) => (
+        <button className="delete-button" onClick={() => handleDelete(params)}>
+          Delete
+        </button>
+      ),
+    },
+  ];
 
   useEffect(() => {
     const getEmployee = async () => {
@@ -72,6 +56,26 @@ export const EmployeeInfo = () => {
     };
     getEmployee();
   }, []);
+
+  const handleUpdate = (params) => {
+    const rowId = params.id; // get the MongoDB _id field
+    const employee = empData.find((emp) => emp.id === rowId);
+    navigate(`/EmployeeAdminPanal/EmployeeInfo/Employee/${rowId}`, { state: { employee } });
+  };
+
+  const handleDelete = async (params) => {
+    const rowId = params.id; // get the MongoDB _id field
+    try {
+      const response = await axios.delete(`/employees/${rowId}`);
+      if (!response) {
+        console.log("not response from delete btn");
+      } else {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="EmployeeInfo">
---
+++ frontend/src/pages/employee/ProUpdateForm/ProUpdateForm.js
@@ -0,0 +1,230 @@
+import React from "react";
+import "./proUpdateForm.scss";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { createMuiTheme, ThemeProvider } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import Radio from "@mui/material/Radio";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { green } from "@mui/material/colors";
+import { SideBar } from "../../../components/EmployeCo/sidebar/SideBar";
+import { NavBar } from "../../../components/EmployeCo/navBar/NavBar";
+import { useLocation, useParams } from "react-router-dom";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  phone: Yup.string().required("Required"),
+  age: Yup.number().required("Required"),
+  role: Yup.string().required("Required"),
+  gender: Yup.string().required("Required"),
+  password: Yup.string().max(20, "Must be 20 characters or less").required("Required"),
+});
+
+const roles = ["HR Manager", "Inventory Manager", "Supplier Manager", "Production Manager"];
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: green["A400"],
+    },
+  },
+});
+
+export const ProUpdateForm = () => {
+  const { id } = useParams();
+  const location = useLocation();
+  const employee = location.state.employee;
+
+  const formik = useFormik({
+    initialValues: {
+      name: employee.name,
+      email: employee.email,
+      phone: employee.phone,
+      gender: employee.gender,
+      age: employee.age,
+      role: employee.role,
+      password: employee.password,
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, { resetForm }) => {
+      try {
+        const response = await axios.put(`/employees/${id}`, {
+          name: values.name,
+          email: values.email,
+          phone: values.phone,
+          gender: values.gender,
+          age: values.age,
+          role: values.role,
+          password: values.password,
+        });
+        if (!response) {
+          console.log("employee not Updated");
+        } else {
+          toast.success("Employee Update Success!", {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          resetForm(); //react formik method
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("User Name Already Taken", {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    },
+  });
+  return (
+    <div className="ProUpdateForm">
+      <SideBar />
+      <div className="profile-contariner">
+        <NavBar />
+        <div className="profile">
+          <ThemeProvider theme={theme}>
+            <form className="profile-form" onSubmit={formik.handleSubmit}>
+              <p className="head-tag-1">update user</p>
+              <p className="head-tag-2">Update Your User Profile</p>
+              <TextField
+                color="primary"
+                fullWidth
+                label="Name"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              />
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              />
+              <TextField
+                fullWidth
+                label="Phone"
+                name="phone"
+                value={formik.values.phone}
+                onChange={formik.handleChange}
+                error={formik.touched.phone && Boolean(formik.errors.phone)}
+                helperText={formik.touched.phone && formik.errors.phone}
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              />
+              <FormControl
+                component="fieldset"
+                sx={{
+                  marginBottom: "1rem",
+                }}
+              >
+                <RadioGroup row name="gender" value={formik.values.gender} onChange={formik.handleChange}>
+                  <FormControlLabel value="male" control={<Radio />} label={<Typography sx={{ fontSize: "13px" }}>Male</Typography>} />
+                  <FormControlLabel value="female" control={<Radio sx={{ fontSize: "13px" }} />} label={<Typography sx={{ fontSize: "13px" }}>Female</Typography>} />
+                </RadioGroup>
+              </FormControl>
+              <TextField
+                fullWidth
+                label="Age"
+                name="age"
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                error={formik.touched.age && Boolean(formik.errors.age)}
+                helperText={formik.touched.age && formik.errors.age}
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              />
+              <FormControl
+                fullWidth
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              >
+                <InputLabel id="role-label">Role</InputLabel>
+                <Select labelId="role-label" label="Role" name="role" value={formik.values.role} onChange={formik.handleChange} error={formik.touched.role && Boolean(formik.errors.role)}>
+                  {roles.map((role) => (
+                    <MenuItem className="menu" key={role} value={role} sx={{ fontSize: "13px" }}>
+                      {role}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <TextField
+                fullWidth
+                type="password"
+                label="Password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+                sx={{
+                  marginBottom: "1rem",
+                  "& .MuiInputBase-root": {
+                    height: "45px", // set the height here
+                  },
+                }}
+              />
+              <Button color="primary" variant="contained" fullWidth type="submit" className="profile-btn" sx={{ marginBottom: "1rem" }}>
+                Submit
+              </Button>
+              <ToastContainer position="top-center" autoClose={3000} limit={1} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+            </form>
+          </ThemeProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
