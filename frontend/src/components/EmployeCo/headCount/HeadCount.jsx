4fb11488dd402361b41f3e6af1fb3be666ff6231
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-03 01:09:36 +0530

    add prograss bar

--- frontend/src/components/EmployeCo/headCount/HeadCount.jsx
+++ frontend/src/components/EmployeCo/headCount/HeadCount.jsx
@@ -1,5 +1,13 @@
 import "./headCount.scss";
+import { PrograssBar } from "../PrograssBar/PrograssBar";
 
 export const HeadCount = () => {
-  return <div className="headCount">HeadCount</div>;
+  return (
+    <div className="headCount">
+      <div className="head-count-probar">
+        <p className="count-heading-1">Head Count</p>
+        <PrograssBar />
+      </div>
+    </div>
+  );
 };
