d031b663a3b1a40548e1dc7c1bbfd3044c9a2571
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-03 17:00:45 +0530

    add create logic

--- backend/controllers/payrollController.js
+++ backend/controllers/payrollController.js
@@ -18,9 +18,37 @@ const getSinglePayroll = (req, res) => {
 //@desc POST create new payrolls
 //@Route POST /api/v1/tea-factory/payrolls
 //@access public
-const createPayroll = (req, res) => {
-  res.status(201).json("create Payroll");
-};
+const createPayroll = asyncHandler(async (req, res) => {
+  const { empName, empId, basicSalary, overtime, bonus, totoalEarnings, fedaralTax, stateTax, medicare, totalDeduction, netPay } = req.body;
+  if (!empName || !empId || !basicSalary || !overtime || !bonus || !totoalEarnings || !fedaralTax || !stateTax || !medicare || !totalDeduction || !netPay) {
+    res.status(400);
+    throw new Error("all fields are mandotory");
+  }
+  const userAvailable = await Payroll.findOne({ empId });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("this name already taken");
+  }
+
+  const payroll = await Payroll.create({
+    empName,
+    empId,
+    basicSalary,
+    overtime,
+    bonus,
+    totoalEarnings,
+    fedaralTax,
+    stateTax,
+    medicare,
+    totalDeduction,
+    netPay,
+  });
+  if (!payroll) {
+    res.status(404);
+    throw new Error("Payroll data not valid");
+  }
+  res.status(201).json(payroll);
+});
 
 //@desc PUT update payrolls
 //@Route PUT /api/v1/tea-factory/payrolls:id
