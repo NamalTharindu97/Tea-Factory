a1c26ed14cd1963b4df41a5f773605852e375e6e
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-06 14:54:32 +0530

    create gender counts

--- frontend/src/components/EmployeCo/pieChart/PieChart.jsx
+++ frontend/src/components/EmployeCo/pieChart/PieChart.jsx
@@ -1,16 +1,33 @@
 import "./pieChart.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pie } from "react-chartjs-2";
 // eslint-disable-next-line no-unused-vars
 import Chart from "chart.js/auto";
+import axios from "axios";
 
 export const PieChart = () => {
+  const [male, setMale] = useState();
+  const [female, setFemale] = useState();
+
+  useEffect(() => {
+    const getGenderCount = async () => {
+      try {
+        const response = await axios.get("/employees/genderCount");
+        setMale(response.data.maleCount);
+        setFemale(response.data.femaleCount);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getGenderCount();
+  }, []);
+
   const data = {
-    labels: ["Red", "Blue"],
+    labels: ["Male", "Female"],
     datasets: [
       {
         label: "# of Votes",
-        data: [19, 3],
+        data: [male, female],
         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
         borderWidth: 1,
@@ -20,14 +37,25 @@ export const PieChart = () => {
 
   const options = {
     plugins: {
-      // title: {
-      //   display: true,
-      //   text: "Pie Chart",
-      // },
+      title: {
+        display: true,
+        text: "Male and Female Distribution",
+        padding: 0,
+        color: "white",
+        font: {
+          family: "Poppins", // replace with your desired font family
+        },
+      },
+
       legend: {
-        position: "top",
+        position: "bottom",
         labels: {
           boxWidth: 15,
+        },
+        color: "white",
+        font: {
+          family: "Poppins", // replace with your desired font family
+          weight: "600", // set font weight to semibold
         },
       },
     },
