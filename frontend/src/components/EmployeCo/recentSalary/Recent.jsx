f1604c30c77613032af20176a5823b6972a5a6c9
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-09 21:52:37 +0530

    add frontend recent tab logic

--- frontend/src/components/EmployeCo/recentSalary/Recent.jsx
+++ frontend/src/components/EmployeCo/recentSalary/Recent.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./recent.scss";
 import { motion } from "framer-motion";
+import axios from "axios";
+import FiberManualRecordTwoToneIcon from "@mui/icons-material/FiberManualRecordTwoTone";
 
 const scaleVariant = {
   initial: { scale: 0 },
@@ -13,9 +16,33 @@ const scaleVariant = {
   },
 };
 export const Recent = () => {
+  const [recentPayroll, setPayrolles] = useState([]);
+
+  useState(() => {
+    const getRecentPayments = async () => {
+      const payrolls = await axios.get("/payrolls/count/Lastest/Payroll");
+      setPayrolles(payrolls.data);
+    };
+    getRecentPayments();
+  }, []);
+
   return (
     <motion.div className="recent" variants={scaleVariant} initial="initial" animate="animate">
-      Recent
+      <p className="recent-heading">Latest Employee Payments</p>
+      {recentPayroll.length > 0 ? (
+        <div>
+          {recentPayroll.map((payroll) => (
+            <div key={payroll._id}>
+              <p className="p-tag-recent payrolls">
+                <FiberManualRecordTwoToneIcon className="recent-icon" />
+                {`Employee ${payroll.empName} Was Paid Rs :  ${payroll.netPay} on ${payroll.createdAt}`}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="p-tag-recent notfound">No Recent Payrolls Found.</p>
+      )}
     </motion.div>
   );
 };
