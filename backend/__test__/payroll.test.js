3eb7b9bb1c87edf27b6fe891cf0d51e05b9de0a9
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-26 19:22:08 +0530

    production backend test cases

--- backend/__test__/payroll.test.js
+++ backend/__test__/payroll.test.js
@@ -1,5 +1,8 @@
 const Payroll = require("../models/payrollModel");
-const app = require("../server"); // Import the app from server.js
+const Employe = require("../models/employeeModel");
+const Production = require("../models/productionModel");
+
+const server = require("../server");
 
 describe("getPayroll", () => {
   it("should return all payrolls", async () => {
@@ -105,5 +108,163 @@ describe("deletePayroll", () => {
   it("should return 404 if payroll is not found", async () => {
     const deletedPayroll = await Payroll.findByIdAndDelete("647086937a12fcb3fd3440d5");
     expect(deletedPayroll).toBeNull();
+  });
+});
+
+//************************************Employee test cases**********************************************************
+
+describe("getEmployee", () => {
+  it("should return all employees", async () => {
+    const employees = await Employe.find();
+    expect(employees).toHaveLength(13);
+  });
+});
+
+describe("createEmployee", () => {
+  it("should create a new Employee", async () => {
+    const employe = {
+      name: "Test User",
+      email: "TestUser@gmail.com",
+      phone: "0772417049",
+      gender: "male",
+      img: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      age: "25",
+      role: "HR Manager",
+      password: "12345",
+    };
+
+    try {
+      const createdEmployee = await Employe.create(employe);
+      expect(createdEmployee).toHaveProperty("name", "Test User");
+    } catch (error) {
+      expect(error.status).toBe(400);
+    }
+  });
+});
+
+describe("getSingleEmploye", () => {
+  it("should return a single payroll by id", async () => {
+    const testUser = await Employe.findOne({ name: "Test User" });
+    const testUserID = testUser._id;
+
+    const employe = await Employe.findById(testUserID);
+    expect(employe).toHaveProperty("name", "Test User");
+  });
+
+  it("should return 404 if payroll is not found", async () => {
+    const payroll = await Employe.findById("64677989f12048fa998ad2de");
+    expect(payroll).toBeNull();
+  });
+});
+
+describe("updateEmploye", () => {
+  it("should update a Employe", async () => {
+    const employe = {
+      name: "Test User",
+      email: "TestUser@gmail.com",
+      phone: "0772417049",
+      gender: "male",
+      img: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      age: "25",
+      role: "Inventory Manager", //updated
+      password: "12345",
+    };
+
+    const testUser = await Employe.findOne({ name: "Test User" });
+    const testUserID = testUser._id;
+
+    const updatedEmploye = await Employe.findByIdAndUpdate(testUserID, employe, {
+      new: true,
+    });
+    expect(updatedEmploye).toHaveProperty("role", "Inventory Manager");
+  });
+});
+
+describe("deleteEmploye", () => {
+  it("should delete a Employe", async () => {
+    const testUser = await Employe.findOne({ name: "Test User" });
+    const testUserID = testUser._id;
+
+    const deletedEmploye = await Employe.findByIdAndDelete(testUserID);
+    expect(deletedEmploye).toBeTruthy();
+  });
+
+  it("should return 404 if payroll is not found", async () => {
+    const deletedEmploye = await Employe.findByIdAndDelete("646a0970e0df889ab9516322");
+    expect(deletedEmploye).toBeNull();
+  });
+});
+
+//************************************Production test cases**********************************************************
+
+describe("createProduction", () => {
+  it("should create a new Employee", async () => {
+    const production = {
+      ProdutionID: "P001-2023",
+      description: "This is a test production",
+      startDate: "2021-09-01",
+      endDate: "2021-09-30",
+      status: "Completed",
+      teaType: "Black Tea",
+      batchNumber: "B001",
+    };
+
+    try {
+      const createdProduction = await Production.create(production);
+      expect(createdProduction).toHaveProperty("ProdutionID", "P001-2023");
+    } catch (error) {
+      expect(error.status).toBe(400);
+    }
+  });
+});
+
+describe("getSingleProduction", () => {
+  it("should return a single Production by id", async () => {
+    const testProduction = await Production.findOne({ ProdutionID: "P001-2023" });
+    const testProductionID = testProduction._id;
+
+    const production = await Production.findById(testProductionID);
+    expect(production).toHaveProperty("ProdutionID", "P001-2023");
+  });
+
+  it("should return 404 if payroll is not found", async () => {
+    const payroll = await Production.findById("6451c5a0bf23f742e7e1f5de");
+    expect(payroll).toBeNull();
+  });
+});
+
+describe("updateProduction", () => {
+  it("should update a Production", async () => {
+    const production = {
+      ProdutionID: "P001-2023",
+      description: "This is a test production and now it updated", //updated
+      startDate: "2021-09-01",
+      endDate: "2021-09-30",
+      status: "Completed",
+      teaType: "Green Tea", //updated
+      batchNumber: "B001",
+    };
+    const testProduction = await Production.findOne({ ProdutionID: "P001-2023" });
+    const testProductionID = testProduction._id;
+
+    const updatedProduction = await Production.findByIdAndUpdate(testProductionID, production, {
+      new: true,
+    });
+    expect(updatedProduction).toHaveProperty("teaType", "Green Tea");
+  });
+});
+
+describe("deleteProduction", () => {
+  it("should delete a Production", async () => {
+    const testProduction = await Production.findOne({ ProdutionID: "P001-2023" });
+    const testProductionID = testProduction._id;
+
+    const deletedProduction = await Production.findByIdAndDelete(testProductionID);
+    expect(deletedProduction).toBeTruthy();
+  });
+
+  it("should return 404 if payroll is not found", async () => {
+    const deletedEmploye = await Production.findByIdAndDelete("6451c5a0bf23f742e7e1f5de");
+    expect(deletedEmploye).toBeNull();
   });
 });
